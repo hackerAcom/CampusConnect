c69d1a15f95c254be590d990395b960ee6cc11a1
hacker <120191235+hackerAcom@users.noreply.github.com>
2024-06-30 22:25:14 +0530

    Add files via upload

---
+++ search.js
@@ -0,0 +1,2 @@
+// This script can be used for search functionality or any additional JS functionality needed
+console.log('Search script loaded');
---
+++ signin.js
@@ -0,0 +1,7 @@
+<script>
+    function signIn() {
+        localStorage.setItem('signedIn', 'true');
+        
+        window.location.href = "sign-in.html";
+    }
+</script>
